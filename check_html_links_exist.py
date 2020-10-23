d203b8044e52b296d32765a5e1bae05b6290b880
Russ Tedrake <russt@mit.edu>
2020-10-23 11:29:20 -0400

    check more html links

--- check_html_links_exist.py
+++ check_html_links_exist.py
@@ -2,6 +2,11 @@ import argparse
 import glob
 import os
 
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from urllib.parse import urljoin
+
 parser = argparse.ArgumentParser(description='Checks my html file links.')
 # Add a workspace argument to support non-hermetic testing through `bazel test`.
 parser.add_argument('--cwd',
@@ -22,9 +27,64 @@ def get_file_as_string(filename):
     return s
 
 
+def getLinksFromHTML(html):
+
+    def getLink(el):
+        return el["href"]
+
+    return list(
+        map(getLink,
+            BeautifulSoup(html, features="html.parser").select("a[href]")))
+
+
+def html_has_id(html, id):
+    tag = BeautifulSoup(html, features="html.parser").find(id=id)
+    return tag is not None
+
+
 # Check that all links to code files exist.
 for filename in glob.glob("*.html"):
     s = get_file_as_string(filename)
+
+    broken_links = []
+
+    for link in getLinksFromHTML(s):
+        if '#' in link:
+            url, id = link.split(sep='#', maxsplit=1)
+        else:
+            url = link
+            id = ''
+        if len(url) == 0:
+            if not html_has_id(s, id):
+                broken_links.append(link)
+        elif url[:4].lower() != 'http':
+            if not os.path.exists(url):
+                broken_links.append(link)
+            # The index is the only case where I want to allow section tags.
+            if filename == 'index.html' and id[:7] == "section":
+                continue
+            if id and not html_has_id(get_file_as_string(url), id):
+                broken_links.append(link)
+        elif url.find("colab.research.google.com") != -1:
+            requestObj = requests.head(link)
+            if requestObj.status_code == 404:
+                broken_links.append(link)
+        else:
+            if id:
+                requestObj = requests.get(link)
+                if requestObj.status_code == 404:
+                    broken_links.append(link)
+                if not html_has_id(requestObj.text, id):
+                    broken_links.append(link)
+            else:
+                requestObj = requests.head(link)
+                if requestObj.status_code == 404:
+                    broken_links.append(link)
+
+    if broken_links:
+        print(f"{filename} has the following broken links:")
+        print("\n".join(broken_links))
+        exit(-2)
 
     for tag in ['jupyter', 'pysrcinclude', 'pysrc']:
         index = 0
