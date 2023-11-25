669dabbb16a25c55dd306db20c8b75570f10590d
Russ Tedrake <russt@mit.edu>
2023-11-25 12:31:10 -0500

    upgrade to python 3.10 minimum

--- PoetryExport.sh
+++ PoetryExport.sh
@@ -3,7 +3,7 @@
 poetry lock
 poetry export --without-hashes --with dev > requirements.txt
 # Work around poetry issue: https://github.com/python-poetry/poetry-plugin-export/issues/176
-sed -E 's/matplotlib==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.8"/matplotlib==3.5.1 ; sys_platform == "linux"\
+sed -E 's/matplotlib==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.10"/matplotlib==3.5.1 ; sys_platform == "linux"\
 matplotlib==3.7.3 ; sys_platform == "darwin"/' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
 # Force torch to be cpu-only for bazel. https://drakedevelopers.slack.com/archives/C2PMBJVAN/p1697855405335329
 awk '
