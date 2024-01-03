a11c43dc4ff945920d4980b49da1f6085e4b8c70
Russ Tedrake <russt@mit.edu>
2024-01-03 09:23:28 -0500

    keep poetry out of the published requirements

--- PoetryExport.sh
+++ PoetryExport.sh
@@ -2,6 +2,9 @@
 
 poetry lock
 poetry export --without-hashes --with dev > requirements.txt
+# Remove poetry. Required to work around poetry <--> poetry-plugin-export circular
+# dependency: https://github.com/python-poetry/poetry-plugin-export/issues/240
+sed '/^poetry/d' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
 # Work around poetry issue: https://github.com/python-poetry/poetry-plugin-export/issues/176
 sed -E 's/matplotlib==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.10"/matplotlib==3.5.1 ; sys_platform == "linux"\
 matplotlib==3.7.3 ; sys_platform == "darwin"/' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
