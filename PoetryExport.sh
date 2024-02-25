37b56527da79ad3b9f19b7e600dbcb1d2f75166c
Russ Tedrake <russt@mit.edu>
2024-02-24 23:36:45 -0500

    export different requirements for bazel mac and linux

--- PoetryExport.sh
+++ PoetryExport.sh
@@ -4,6 +4,8 @@
 poetry export --without-hashes --with dev > requirements.txt
 # Remove poetry. Required to work around poetry <--> poetry-plugin-export circular
 # dependency: https://github.com/python-poetry/poetry-plugin-export/issues/240
+# Note that pip_parse does provide a mechanism for handling known cycles:
+# https://github.com/bazelbuild/rules_python/blob/9a1a524cb0f9c048fbd0abc9837d4a757d98b9c5/examples/pip_parse/WORKSPACE#L27
 sed '/^poetry/d' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
 # Work around poetry issue: https://github.com/python-poetry/poetry-plugin-export/issues/176
 sed -E 's/matplotlib==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.10"/matplotlib==3.5.1 ; sys_platform == "linux"\
@@ -18,3 +20,8 @@ awk '
     next;
 }
 1' requirements.txt > requirements-bazel.txt
+# Embarassingly, pip_parse does not support markers, so we need to split the
+# requirements into separate files.
+# https://github.com/bazelbuild/rules_python/issues/1105
+awk '/sys_platform == "darwin"/ || !/sys_platform/' requirements-bazel.txt > requirements-bazel-mac.txt
+awk '/sys_platform == "linux"/ || !/sys_platform/' requirements-bazel.txt > requirements-bazel-linux.txt
