ec25c9d4a32cdd93e49d290fb2507357a2e59981
Russ Tedrake <russt@mit.edu>
2024-12-28 11:35:49 -0500

    massage poetry export to support ubuntu 24.04

--- PoetryExport.sh
+++ PoetryExport.sh
@@ -7,9 +7,6 @@ poetry export --without-hashes --with dev > requirements.txt
 # Note that pip_parse does provide a mechanism for handling known cycles:
 # https://github.com/bazelbuild/rules_python/blob/9a1a524cb0f9c048fbd0abc9837d4a757d98b9c5/examples/pip_parse/WORKSPACE#L27
 sed '/^poetry/d' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
-# Work around poetry issue: https://github.com/python-poetry/poetry-plugin-export/issues/176
-sed -E 's/matplotlib==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.10"/matplotlib==3.5.1 ; sys_platform == "linux"\
-matplotlib==3.7.3 ; sys_platform == "darwin"/' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
 # Force torch to be cpu-only for bazel. https://drakedevelopers.slack.com/archives/C2PMBJVAN/p1697855405335329
 awk '
 /torch==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.10"/ {
@@ -34,3 +31,7 @@ awk '
 # https://github.com/bazelbuild/rules_python/issues/1105
 awk '/sys_platform == "darwin"/ || !/sys_platform/' requirements-bazel.txt > requirements-bazel-mac.txt
 awk '/sys_platform == "linux"/ || !/sys_platform/' requirements-bazel.txt > requirements-bazel-linux.txt
+# Soften the matplotlib requirement to >= for bazel on linux, since we get
+# interference with the matplotlib installed via apt python3-matplotlib.
+# https://github.com/RobotLocomotion/drake/issues/14250#issuecomment-984217002  
+sed -E 's/matplotlib==[0-9]+\.[0-9]+\.[0-9]+/matplotlib>=3.5.1/' requirements-bazel-linux.txt > requirements-bazel-linux.txt.tmp && mv requirements-bazel-linux.txt.tmp requirements-bazel-linux.txt
